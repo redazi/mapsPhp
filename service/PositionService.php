66fc9d4266f02fddb2e06e43e93f381cc45753ca
redazi <zidahireda97@gmail.com>
2022-04-20 00:23:48 +0000

    first commit

---
+++ service/PositionService.php
@@ -0,0 +1,42 @@
+<?php
+include_once 'dao/IDao.php';
+include_once 'classe/Position.php';
+include_once 'connexion/Connexion.php';
+class PositionService implements IDao
+{
+    private $listPosition = array();
+    private $connexion;
+    private $position;
+    public function __construct()
+    {
+        $this->connexion = new Connexion();
+        $this->position = new Position("", "", "", "", "");
+    }
+    public function create($position)
+    {
+        $query = "INSERT INTO geolocalisation (latitude, longitude, date, imei) VALUES ("
+            . $position->getLatitude() . "," . $position->getLongitude() . ",'" . $position->getDate() . "','" . $position->getImei() . "')";
+        $req = $this->connexion->getConnextion()->prepare($query);
+        $req->execute() or die('SQL');
+    }
+    public function delete($obj)
+    {
+    }
+  
+     public function findAll() {
+        
+ $query = "select * from geolocalisation";
+ $req = $this->connexion->getConnextion()->prepare($query);
+ $req->execute();
+ return $req->fetchAll(PDO::FETCH_ASSOC);
+ 
+        
+    }
+
+    public function getById($obj)
+    {
+    }
+    public function update($obj)
+    {
+    }
+}
